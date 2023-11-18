9618f3b35db3c6d65d84576add2f94cc54158d3f
roystiven <roy_stiven@hotmail.com>
2023-11-17 21:40:05 -0500

    first merge

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "navbar/Navbar"
+import Counter from "CounterReact/CounterReact"
+import "./index.css";
+
+const App = () => (
+  <Fragment>
+    <Navbar>
+    </Navbar>
+    hliasdasd
+    <Counter/>
+  </Fragment>
+);
+
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
